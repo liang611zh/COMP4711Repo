74e6bf68487c59a404247b82a41c617fbbc9e40b
Liang Zhao <zhaoliangbz@gmail.com>
2017-09-07 22:04:39 -0700

    modify student class and modify the index page

--- Student.php
+++ Student.php
@@ -1,28 +1,44 @@
 <?php
-/**
-*@author liang zhao
-*
-*
-*/
+	/**
+	*@author liang zhao
+	*
+	*
+	*/
 class Student{
+	/**
+	*constructor
+	*
+	*
+	*/
 	function __construct(){
 		$this->surname = '';
 		$this->first_name = '';
 		$this->emails = array();
 		$this->grades = array();
 	}
-	
-	function add_email($which,$address){
+	/**
+	*add email function
+	*
+	*
+	*/	
+	function add_email($which,$address) {
 		$this->emails[$which] = $address;
-		
-		
 	}
 	
+	/**
+	*add grade function
+	*
+	*
+	*/
 	function add_grade($grade) {
-		$this_grades[]=-$grade;
-		
+		$this->grades[] = $grade;
 	}
 	
+	/**
+	*calculate average score
+	*
+	*
+	*/	
 	function average() {
 		$total = 0;
 		foreach ($this->grades as $value)
@@ -30,12 +46,17 @@ class Student{
 		return $total / count($this->grades);
 	}
 	
+	/**
+	*print method
+	*
+	*
+	*/	
 	function toString() {
 		$result = $this->first_name . ' ' . $this->surname;
-		$result .= ' ('.$this->average().")\n";
+		$result .= ' ('.$this->average().")\n\n";
 		foreach($this->emails as $which=>$what)
-			$result .= $which . ': '. $what. "\n";
-		$result .= "\n";
+			$result .= $which . ': '. $what. "\n\n";
+		$result .= "\n\n";
 		return '<pre>'.$result.'</pre>';
 	}
 }
--- index.php
+++ index.php
@@ -2,11 +2,63 @@
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>123</title>
-	</head>
+		<title>COMP4711 LAB1 Liang Zhao</title>
+		<style>
+		body {
+				text-align: center;
+				background-color: powderblue;
+				color: black;
+				font-family: courier;
+				font-size: 20px;
+		}
+		</style>
 	<body>
 		<?php
-		echo "Hello out there ... I live";
+		include('Student.php');
+		
+		//student 1
+		$students = array();
+		$first = new Student();
+		$first->surname = "Doe";
+		$first->first_name = "John";
+		$first->add_email('home','john@doe.com');
+		$first->add_email('work','jdoe@mcdonalds.com');
+		$first->add_grade(65);
+		$first->add_grade(75);
+		$first->add_grade(55);
+		$students['j123'] = $first;
+		
+		//student 2
+		$second = new Student();
+		$second->surname = "Einstein";
+		$second->first_name = "Albert";
+		$second->add_email('home','albert@braniacs.com');
+		$second->add_email('work1','a_einstein@bcit.ca');
+		$second->add_email('work2','albert@physics.mit.edu');
+		$second->add_grade(95);
+		$second->add_grade(80);
+		$second->add_grade(50);
+		$students['a456'] = $second;
+		
+		//student 3
+		$third = new Student();
+		$third->surname = "Zhao";
+		$third->first_name = "Liang";
+		$third->add_email('home','liang@braniacs.com');
+		$third->add_email('work1','lzhao51@bcit.ca');
+		$third->add_email('work2','zhaoliang@physics.mit.edu');
+		$third->add_grade(96);
+		$third->add_grade(98);
+		$third->add_grade(100);
+		$students['z789'] = $third;
+		
+		//sort student 
+		ksort($students);
+		
+		//print student information
+		foreach($students as $student)
+			echo $student->toString();
+
 		?>
 	</body>
 </html>
